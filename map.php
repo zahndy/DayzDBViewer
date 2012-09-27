33b42dc6f3451a02157be1fb2dfd6ed0aeedf5db
zahndy <zahndy@gmail.com>
2012-09-27 09:49:27 -0800

    added zoom levels to vehicles map
    also changed map image.

--- map.php
+++ map.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -6,9 +5,17 @@
         <meta charset="utf-8" />
         <title></title>
     </head>
-    <body  id="mapbody">
-	<img src="chernarus.jpg" />
+	 <body id="mapbody" >
+	<div id="zoom">zoom: <a href="map.php?zoom=1" target="mainframe" class="button">1x</a> 
+	<a href="map.php?zoom=2" target="mainframe" class="button">2x</a> 
+	<a href="map.php?zoom=4" target="mainframe" class="button">3x</a> 
+	</div>
+						
 <?php
+if($_REQUEST["zoom"]=="4"){ echo '<img src="map_2.jpg" width="4088px" height="3405px" />';}
+else if( $_REQUEST["zoom"]=="2"){ echo '<img src="map_1.jpg" width="2040px" height="1702px" />';}
+else{ echo '<img src="map_0.jpg" width="1020px" height="851px" />';}
+
 require "config.php";
 
 $cxn = mysqli_connect(_host, _user, _password, _database)
@@ -39,14 +46,26 @@ ORDER BY otype";
 
 $result = mysqli_query($cxn,$query)
 				or die("couldnt execute query");
-
+				
 while ($row= mysqli_fetch_assoc($result))
 		{
 			$ob = process($row['pos'],$row['otype']);
-			echo "<img src='".$ob[2]."' alt='".$ob[3]."' title='".$ob[3]."' style=\"position:absolute; top:".(string)(1580-($ob[1]/10)). "px; left:".(string)(($ob[0]/10)-30). "px; \"  /><br/>";
+			
+			// some wierd ass numbers and offsets that dont make sense at all but it works http://i.qkme.me/355ovv.jpg
+			if($_REQUEST["zoom"]=="4"){
+			echo "<img src='".$ob[2]."' alt='".$ob[3]."' title='".$ob[3]."' style=\"position:absolute; top:".(string)(((13700-$ob[1])/3.54)-85). "px; left:".(string)(($ob[0]/3.54)-95). "px; \"  />";
+			}
+			else if( $_REQUEST["zoom"]=="2"){ 
+			echo "<img src='".$ob[2]."' alt='".$ob[3]."' title='".$ob[3]."' style=\"position:absolute; top:".(string)(((13700-$ob[1])/7.08)-42.5). "px; left:".(string)(($ob[0]/7.08)-47.5). "px; \"  />";
+			}
+			else{ 
+			echo "<img src='".$ob[2]."' alt='".$ob[3]."' title='".$ob[3]."' style=\"position:absolute; top:".(string)(((13700-$ob[1])/14.16)-21.25). "px; left:".(string)(($ob[0]/14.16)-23.75). "px; \"  />";
+			}
 		}
 	mysqli_close($cxn);
 
+
+	
 function process($str,$name)
 {
 
@@ -59,6 +78,8 @@ function process($str,$name)
     array_push($arr,$name); 
     return $arr; // [posx,posy,image,name]
 }
+
 ?>
     </body>
+	
 </html>
